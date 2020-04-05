be793c5fccb6f9bb5a2a1beccad6f7e2de58dcb2
badari07 <badari0793@gmail.com>
2020-04-06 00:19:05 +0530

    setup react

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+//import { Router } from "@reach/router";
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
